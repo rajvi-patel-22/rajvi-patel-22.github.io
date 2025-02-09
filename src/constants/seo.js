d24563695a110c68f239ccd65582482568259823
Rajvi Patel <rajvi2232000@gmail.com>
2025-02-09 13:20:19 -0700

    Ass seo

---
+++ src/constants/seo.js
@@ -0,0 +1,39 @@
+const page_meta = {
+	titlePrefix: '',
+	titleSuffix: ' - Rajvi Patel',
+	twitterUser: 'rameskum',
+	pages: [
+		{
+			id: 'home',
+			title: 'Home',
+			description:
+				"I am a developer with a keen interest in technology, who doesn't fear computers rather fear the lack of them.",
+			paths: ['/', ''],
+			image: 'home.png',
+			type: 'website',
+		},
+		{
+			id: 'about',
+			title: 'About Me',
+			description: 'Learn more about Rajvi',
+			paths: ['/about', '/about/'],
+			type: 'website',
+		},
+		{
+			id: 'projects',
+			title: 'Projects',
+			description: 'Projects developed by Rajvi.',
+			paths: ['/projects', '/projects/'],
+			type: 'website',
+		},
+		{
+			id: 'contact',
+			title: 'Contact',
+			description: 'Contact details of Rajvi.',
+			paths: ['/contact', '/contact/'],
+			type: 'website',
+		},
+	],
+};
+
+export default page_meta;
